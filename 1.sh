730e1d798e6e2040c22d430d3d0c86ae538105fc
TheScarletArrow <anton20001701@yandex.ru>
2019-11-01 21:39:39 +0300

    first commit

---
+++ 1.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+if [-d $1] && [-d $2]
+then
+	echo "Dirs exist"
+elif [! -d $2] 
+	echo "Flash path doesn't exist"
+	mkdir $2
+fi
+
+for file in $1/*
+do
+	echo $(basename $file)
+	flashfile=$($2/$(basename $file))
+	if [! -f $flashfile]; then
+		echo "Copying files to $2"
+		cp $file $2
+	
+	elif [ $(ls -l $file) qt $(ls -l $flashfile) ]
+		echo "Flash version is newer, replace existing one? Y\N"
+
+	read reply
+	case $reply in
+	y|Y) echo "Copying now..."
+		cp $flashdrive $1
+	;;
+	n|N) echo "Staying as we were" 
+		exit 2
+	*) echo "Wrong input"
+	;;
+	esac
+	fi
+done
+		
+
