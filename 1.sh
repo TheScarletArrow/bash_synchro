e6f6b75bce15bb406ab637d1a443391f2aa6e71c
terminalforlife <terminalforlife@yahoo.com>
2019-11-07 16:43:00 +0000

    Resolve conflict after merge with master (+more)
    Regarding the "+more" part:
    
    I may not have realised recently, when I made changes to this file, -
    that you were in-fact using the Bourne Shell (`sh`) _not_ the Bourne
    Again Shell (`bash`), so I've undone my changes and hopefully once
    again made it compatible with the correct shell. Sorry about that.
    
    There were some bizarre quotations when you used `echo`, so I sorted
    that out, and I think it was down to you either being confused about
    rules and best practices with variables, or you wanted to have the user
    see the contents of the variables _quoted_, for easy distinction; with
    the latter in mind, I've single-quoted them, as seems commonplace.
    
    Incorrect (albeit technically valid _here_) quotation:
    
      echo "this is a "$variable" which holds stuff"
    
    Correct quotation:
    
      echo "this is a \"$variable\" which holds stuff"
    
    Also correct, if preferred:
    
      echo "this is a '$variable' which holds stuff"
    
    So why was the first one incorrect, yet still displayed?
    
    It's partly down to how echo works, but mainly down to how shell works.
    You've probably already discovered that things in shell should be
    space-delimited, as that's usually how they are, by default, -
    interpreted, but in this case, assuming the variable isn't assigned a
    value with spaces or shell-interpretable characters, it's still valid.
    
    Ordinarily, however, this may be a risky crutch to lean on, so it's
    usually best to play it safe, in this regard.
    
    Admittedly, it's not black and white, but I imagine after a while, -
    you'll see what I'm getting at. ;)

--- 1.sh
+++ 1.sh
@@ -1,34 +1,51 @@
 #! /bin/sh
 
-if [ -d "$1" ] && [ -d "$2" ]
+# $1 is first directory somewhere on PC
+# $2 external directory, flash drive
+
+#$2="/media/anton/YURKOVANTON"
+#$flashfile - file in flash directory
+#$file - file
+if [ ! -d  "$2" ]
 then
-	echo "Dirs exist"
-elif [ ! -d "$2" ] 
-	echo "Flash path doesn't exist"
+	echo "flash directory does not exist"
+	echo "creating '$2' path in flash drive"
 	mkdir "$2"
+	echo "Directory '$2' successfully made on your flash drive"
+	echo "Will be copying from '$1' to '$2'"
 fi
 
+# Below line will require NO trailing forward-slash from user input.
 for file in "$1"/*
 do
-	echo "$(basename $file)"
-	flashfile="$2/$(basename $file)"
+	echo "File '$(basename "$file")' "
+	#echo "$(basename $file)"
 
-	if [ ! -f "$flashfile" ]; then
-		echo "Copying files to '$2'"
+	flashfile="$2/$(basename "$file")"
+
+	if [ ! -f "$flashfile" ]
+	then
+		echo "No such files on flash drive"
+		echo "Copying 'em to '$2'"
 		cp "$file" "$2"
-	
-	elif [ $(ls -l $file) qt $(ls -l $flashfile) ]
-		read -e -n 1 -p "Flash version is newer, replace existing one? [Y|N] "
-		case "$REPLY" in
-			y|Y)
-				echo "Copying now..."
-				cp "$flashdrive" "$1" ;;
-			n|N)
-				echo "Staying as we were" 
+
+	elif [ -f "$flashfile" ] && [ "$file" -ot "$flashfile" ]
+	then
+		echo "Flash version is newer -- replace existing one? [Y|N] "
+		read reply
+		case "$reply" in
+			[yY]|[yY][eE][sS])
+				echo "Copying now to '$1'"
+				cp "$file" "$1" ;;
+			[nN]|[nN][oO])
+				echo "Staying as we were"
 				exit 2 ;;
 			*)
 				echo "Wrong input" ;;
 		esac
+
+	else
+		cp "$file" "$2"
 	fi
 done
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # bashone
 # This is my attempt to make bash file for lab work
 # It is currently in progress
+# my goal is to syncronise two paths
+# path 1 and path 2
+# from my point of view there are 2 ways
+# 1st is when file is on PC but not on flash drive - then we just copy it to flash
+# 2nd is when we have these files both on PC and flash, we need to find out wheter is newer, and replace the older
