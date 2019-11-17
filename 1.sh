190a3b2d2605bd57f7252d55d5b3debce4be0cf7
TheScarletArrow <56115778+TheScarletArrow@users.noreply.github.com>
2019-11-17 17:48:23 +0300

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -3,47 +3,58 @@
 # $1 is first directory somewhere on PC
 # $2 external directory, flash drive
 
-#$2="/media/anton/YURKOVANTON"
-#$flashfile - file in flash directory
-#$file - file
-if [ ! -d  "$2" ]; then
-	echo "flash directory does not exist"
-	echo "creating "$2" path in flash drive"	
-	mkdir "$2"
-	echo "Directory "$2" successfully made on your flash drive"
-	echo "Will be copying from "$1" to "$2""
+Syncro()
+{
+local dir1=$1
+local dir2=$2
+
+for i in "$dir1"/*
+do
+	i=${i##*/}
+
+	echo "\n Current file "$dir1/$i" \n"
+	
+	if [ -f "$dir2/$i" ] && [ -f "$dir1/$i" ]; then
+	echo "\t" 1 Date and time of latest modification of "$dir1/$i" is $(date -r $dir1/$i)
+	echo "\t" 2 Date and time of latest modification of "$dir2/$i" is $(date -r $dir2/$i)
+	exec 3>&1
+	
+	result=$(gdialog --title "Warning" --menu "Which file to copy?" 15 80 2 1 "$dir1/$i: $(date -r "$dir1/$i")"  2 "$dir2/$i: $(date -r "$dir2/$i")"  2>&1 1>&3)
+	case "$result" in
+		'1') cp "$dir1/$i" "$dir2/$i";;
+		'2') cp "$dir2/$i" "$dir1/$i";;
+	esac
+	echo "\n"
+	exec 3>&-
+	continue
+elif [ -d "$dir2/$i" ] && [ -d "$dir1/$i" ]; then
+	Syncro "$dir1/$i" "$dir2/$i"
+	echo "Heading to $i subdirectory"	
+	continue	
+fi
+cp -r "$dir1/$i" "$dir2/$i"
+done
+
+for j in "$dir2"/*
+do
+j=${J##*/}
+echo "File" $dir2/$j
+if [ ! -e "$fir2/$j" ]; then
+	echo "doesnt exist in dir1 $dir2/$j"
+	cp -r "$dir2/$j" "$di1/$j"
+fi
+done
+echo "\n"
+}
+
+directory1="/home/anton/Desktop/lab"
+directory2="/home/anton/Desktop/folder"
+
+if [ ! -d "$directory1" ]; then
+	echo "directory $directory1 does not exist"
 fi
 
-for file in "$1"/*
-do
-	echo "File "$(basename $file)" "
-	#echo "$(basename $file)"
-
-	$flashfile="$2/$(basename $file)"
-
-	if [ ! -f "$flashfile" ]
-	then
-		echo "No such files on flash drive"
-		echo "Copying 'em to "$2""
-		cp "$file" "$2"
-	
-	elif [ -f $flashfile ] && [ "$(ls -l $file)" lt "$(ls -l $flashfile)" ];
-		then
-		#echo "Flash version is newer, replace existing one? Y\N"
-		read  -e -n 1 -p reply "Flash version is newer, replace existing one? [Y|N]"
-		#read reply
-		case $reply in
-			y|Y) echo "Copying now to "$1" "
-			cp "$file" "$1";;
-			n|N) echo "Staying as we were" 
-			exit 2 ;;
-			*) echo "Wrong input"
-		;;
-		esac
-		
-	else
-			cp "$file" "$2"
-	fi
-done
-	
-
+if [ ! -d "$directory2" ]; then
+	echo "directory $directory2 does not exist"
+fi
+[ -d "$directory1" ] && [ -d "$directory2" ] && Syncro $directory1 $directory2
